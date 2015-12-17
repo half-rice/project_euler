10b2e0c2b41b477c0bacaeb5e61cd15e165e7f28
surrus <xsurrus@gmail.com>
2015-12-17 07:40:20 -0700

    sp

--- p003.c
+++ p003.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+int prime_factor(int number);
+
 /*
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 int main(){
+  int i;
+  int primes[100];
 
   return 0;
+}
+
+int prime_factor(int n){
+  int i;
+
+  i = 0;
+  for(i = 2; i <=n; ++i){
+
+  }
+
+}
+
+int is_prime(int n){
+  int i;
+
+  i = 0;
+  if((n % 2) == 0 && i > 2){
+    return 0;
+  }
+  else if(i )
 }
