1a87822e5d6a4ce2a5a07cd529521c1750de1a2c
surrus <xsurrus@gmail.com>
2015-12-19 05:52:01 -0700

    weird ass bug when iterating through primes not printing correctly without an arbritrary printf(n) statement to close the for loop. printing strange numbers when expecting the first statement printf(%d, i) to simply print to iteration number. need more coffee

--- p003.c
+++ p003.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
 int prime_factor(int number);
+int is_prime(int number);
 
 /*
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 int main(){
-  int i;
+  int i, bIsPrime;
   int primes[100];
+
+  i = 0;
+  for(i = 0; i < 25; ++i){
+    printf("%d", i);
+    if(is_prime(i) == 1){
+      printf(" is prime\n");
+    } 
+    printf("\n");
+  }
 
   return 0;
 }
 
+/*
 int prime_factor(int n){
   int i;
 
@@ -20,15 +32,25 @@ int prime_factor(int n){
   for(i = 2; i <=n; ++i){
 
   }
-
 }
+*/
 
 int is_prime(int n){
   int i;
 
-  i = 0;
-  if((n % 2) == 0 && i > 2){
+  if((n % 2) == 0 && n > 3){
     return 0;
   }
-  else if(i )
+
+  i = 0;
+  for(i = 2; i <= n-1; ++i){
+    if(n % i == 0){
+      return 0;
+    }
+  }
+  if(i == n){
+    return 1;
+  }
+
+  return 0;
 }
